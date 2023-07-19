4c5e5d5a985c8afce0594758920fa0498ec43b94
Burger-karl <charleskalu863@gmail.com>
2023-07-19 14:55:44 +0100

    final changes made

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Vue Todos</h1>
+
+    <p id="words">
+      Vue Todos is a CRUD Todo tracking application created by Charles Ifeanyi.<br><br>
+      Track your daily task's using this todo tracker with abilities to save, complete, edit & delete todos.<br><br>
+      Feel free to add on to this application with functionalities such as the ability to drag and reorder 
+      todos, animate creation/deletion of todos or even create this into a full stack application with 
+      the ability to have different lists for different days of tracking todos!
+    </p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
+
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    display: block;
+    margin: 20px 20%;
   }
-}
+
+  .about h1 {
+    margin-bottom: 20px; 
+  }
+
+  .about #words {
+    line-height: 25px;
+    font-weight: 500;
+  }
+
 </style>
